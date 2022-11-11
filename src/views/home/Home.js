043b7dba54edacea224997cf0cf6b419ec9241e9
Umair Hassan <88096624+UmairHassan1122@users.noreply.github.com>
2022-11-11 21:41:12 +0500

    Completed

--- src/views/home/Home.js
+++ src/views/home/Home.js
@@ -4,36 +4,123 @@ import AntDesign from 'react-native-vector-icons/AntDesign'
 import PrefHandler from '../../data/local/PrefHandler'
 import { useFocusEffect } from '@react-navigation/native'
 import Input from '../../reuseables/Input'
+import LoadingPage from '../../reuseables/LoadingPage'
 
 export default function Home({ navigation }) {
     const [colorsShow, setColorsShow] = useState(false)
     const [vaccineDetails, setVaccineDetails] = useState(false)
-    const [potion1, setPotion1] = useState(false)
-    const [potion2, setPotion2] = useState(false)
-    const [potion3, setPotion3] = useState(false)
     const [note, setNote] = useState(false)
     const [noteCheck, setNoteCheck] = useState(false)
-    const [noteInput, setNoteInput] = useState('')
+    const [loading, setLoading] = useState(false)
     const [data, setData] = useState([])
     const [selectedIndex, setSelectedIndex] = useState([-1])
+    const [email, setEmail] = useState('')
+    const [name, setName] = useState('')
+    const [age, setAge] = useState('')
+    const [hivCheck, setHivCheck] = useState(false)
+    const [selectedOpenBox, setselectedOpenBox] = useState(-1)
+    const [inputValue, setinputValue] = useState("")
+    const [inputValueIndex, setinputValueIndex] = useState(-1)
+
     const prefHandle = new PrefHandler()
 
     useEffect(() => {
+        setLoading(true)
         prefHandle.getSession((onSucess) => {
-            console.log(JSON.stringify(onSucess.userInfo.data))
             setData(onSucess.userInfo.data)
-        })
+            setEmail(onSucess.userInfo.email)
+            setName(onSucess.userInfo.name)
+            setAge(onSucess.userInfo.age)
+            setHivCheck(onSucess.userInfo.hivCheck)
+        setLoading(false)
+    })
     }, [])
 
     useFocusEffect(
         React.useCallback(() => {
             prefHandle.getSession((onSucess) => {
-                console.log(JSON.stringify(onSucess.userInfo.data))
                 setData(onSucess.userInfo.data)
+                setData(onSucess.userInfo.data)
+                setEmail(onSucess.userInfo.email)
+                setName(onSucess.userInfo.name)
+                setAge(onSucess.userInfo.age)
+                setHivCheck(onSucess.userInfo.hivCheck)
             });
 
         }, [])
     );
+
+    const OpenDetails = (index) => {
+        if (index == selectedIndex) {
+            setVaccineDetails(false)
+        } else {
+            setSelectedIndex(index)
+            setVaccineDetails(true)
+        }
+    }
+
+    const StoreNote = (item) => {
+        setData(prevState => {
+            const newState = prevState.map(obj => {
+
+                if (obj.id === item.id) {
+                    return { ...obj, note: inputValue };
+                }
+
+                return obj;
+            });
+
+            const body = ({
+                name: name,
+                email: email,
+                age: age,
+                hivCheck: hivCheck,
+                data: newState,
+            });
+
+            prefHandle.createSession(body,
+                (onSucess) => {
+                    console.log(onSucess)
+                },
+                (error) => {
+                    console.log(error)
+                }
+            )
+            return newState;
+        });
+    }
+
+
+    const StoreDose = (item, valueToAssign, valueToUSe) => {
+        setData(prevState => {
+            const newState = prevState.map(obj => {
+
+                if (obj.id === item.id) {
+                    return { ...obj, [valueToAssign]: valueToUSe };
+                }
+
+                return obj;
+            });
+
+            const body = ({
+                name: name,
+                email: email,
+                age: age,
+                hivCheck: hivCheck,
+                data: newState,
+            });
+
+            prefHandle.createSession(body,
+                (onSucess) => {
+                    console.log(onSucess)
+                },
+                (error) => {
+                    console.log(error)
+                }
+            )
+            return newState;
+        });
+    }
 
     const VaccineView = ({ item, index }) => {
         return (
@@ -43,7 +130,8 @@ export default function Home({ navigation }) {
                     <View style={{ backgroundColor: item.color, padding: 10, borderRadius: 45 }} />
                 </View>
 
-                <View style={{ marginTop: 10, flexDirection: 'row', alignItems: 'center', justifyContent: 'space-around', marginLeft: 20, marginRight: 47 }}>
+                {/* <View style={{ marginTop: 10, flexDirection: 'row', alignItems: 'center', justifyContent: 'space-around', marginLeft: 20, marginRight: 47 }}>
+                  
                     <TouchableOpacity style={{ backgroundColor: '#F2F3F7', width: 94, padding: 15, borderRadius: 5 }} onPress={() => setPotion3(!potion3) + setSelectedIndex(index)}>
                         {potion3 && index == selectedIndex ?
                             <AntDesign name={'checkcircleo'} size={18} color={'#000'} style={{ alignSelf: 'center' }} />
@@ -64,25 +152,81 @@ export default function Home({ navigation }) {
                             :
                             <Text style={{ color: '#000', fontFamily: "OpenSans-Medium", marginRight: 5, fontSize: 14, textAlign: 'center' }}>מנה 1</Text>}
                     </TouchableOpacity>
+                </View> */}
+
+                <View style={{ marginTop: 10, flexDirection: 'row', alignItems: 'center', justifyContent: 'space-around', marginLeft: 20, marginRight: 47 }}>
+
+                    <TouchableOpacity style={{ backgroundColor: '#F2F3F7', width: 94, padding: 15, borderRadius: 5 }} onPress={() => {
+                        StoreDose(item, "dose1", !item?.dose1)
+                    }}>
+                        {item?.dose1 ?
+                            <AntDesign name={'checkcircleo'} size={18} color={'#000'} style={{ alignSelf: 'center' }} />
+                            :
+                            <Text style={{ color: '#000', fontFamily: "OpenSans-Medium", marginRight: 5, fontSize: 14, textAlign: 'center' }}>מנה 3</Text>}
+                    </TouchableOpacity>
+
+                    <TouchableOpacity style={{ backgroundColor: '#F2F3F7', width: 94, padding: 15, borderRadius: 5 }} onPress={() => {   StoreDose(item, "dose2", !item?.dose2)}}>
+                        {item?.dose2 ?
+                            <AntDesign name={'checkcircleo'} size={18} color={'#000'} style={{ alignSelf: 'center' }} />
+                            :
+                            <Text style={{ color: '#000', fontFamily: "OpenSans-Medium", marginRight: 5, fontSize: 14, textAlign: 'center' }}>מנה 2</Text>}
+                    </TouchableOpacity>
+
+                    <TouchableOpacity style={{ backgroundColor: '#F2F3F7', width: 94, padding: 15, borderRadius: 5 }} onPress={() => {  StoreDose(item, "dose3", !item?.dose3) }}>
+                        {item?.dose3 ?
+                            <AntDesign name={'checkcircleo'} size={18} color={'#000'} style={{ alignSelf: 'center' }} />
+                            :
+                            <Text style={{ color: '#000', fontFamily: "OpenSans-Medium", marginRight: 5, fontSize: 14, textAlign: 'center' }}>מנה 1</Text>}
+                    </TouchableOpacity>
                 </View>
 
-                {note && index == selectedIndex ?
-                    <View>
-                        <Input bgStyle={{ marginRight: 30 }} multiline={true} edit={noteCheck} onChange={(txt) => { setNoteInput(txt) }} onEndEditing={() => { setNoteInput(noteInput) }} />
-                        {noteCheck ?
-                            <TouchableOpacity style={{ marginTop: 5, padding: 5 }} onPress={() => setNoteCheck(false)}>
-                                <AntDesign name={'checkcircleo'} size={25} color={'#000'} style={{ marginLeft: 20 }} />
-                            </TouchableOpacity>
-                            :
-                            null
-                        }
-                    </View>
-                    :
-                    null}
+                {(item.note != "" && selectedIndex != index) &&
+                    <Input bgStyle={{ marginRight: 30 }} multiline={true} edit={noteCheck} val={item.note} onChange={(txt) => { }} />}
+
+
+                {
+                    note && index == selectedIndex ?
+                        <View>
+                            {/* <Input bgStyle={{ marginRight: 30 }} multiline={true} edit={noteCheck} val={item.note + noteInput} onChange={(txt) => { setNoteInput(txt) }} /> */}
+                            <Input bgStyle={{ marginRight: 30 }} multiline={true} edit={noteCheck} val={(inputValue != "" && inputValueIndex == index) ? inputValue : item.note} onChange={(txt) => {
+                                // const [inputValue, setinputValue] = useState("")
+                                // const [inputValueIndex, setinputValueIndex] = useState(-1)
+
+                                setinputValue(txt)
+                                setinputValueIndex(index)
+
+
+
+                                // setNoteInput(txt)
+
+                            }} />
+                            {noteCheck ?
+                                <TouchableOpacity style={{ marginTop: 5, padding: 5 }} onPress={() => {
+                                    setNoteCheck(false)
+                                    if (inputValue != "") {
+                                        StoreNote(item)
+                                    }
+                                }}>
+                                    <AntDesign name={'checkcircleo'} size={25} color={'#000'} style={{ marginLeft: 20 }} />
+                                </TouchableOpacity>
+                                :
+                                null
+                            }
+                        </View>
+                        :
+                        null
+                }
 
                 <View style={{ marginLeft: 20, marginRight: 47, marginTop: 15, flexDirection: 'row', alignItems: 'center', justifyContent: 'space-between' }} >
                     <View style={{}}>
-                        <TouchableOpacity activeOpacity={.3} style={{ flexDirection: 'row', alignItems: 'center', }} onPress={() => setVaccineDetails(!vaccineDetails) + setSelectedIndex(index)}>
+                        <TouchableOpacity activeOpacity={.3} style={{ flexDirection: 'row', alignItems: 'center', }} onPress={() => {
+                            // OpenDetails(index)
+                            if (selectedOpenBox == index) {
+                                setselectedOpenBox(-1)
+                            } else {
+                                setselectedOpenBox(index)
+                            }
+                        }}>
                             <Text style={{ color: '#000', fontFamily: "OpenSans-Medium", marginRight: 6, fontSize: 14, }}>למידע נוסף</Text>
                             <AntDesign name={vaccineDetails && index == selectedIndex ? 'up' : 'down'} size={14} color={'#000'} style={{ marginRight: 6 }} />
                         </TouchableOpacity>
@@ -91,12 +235,21 @@ export default function Home({ navigation }) {
                     <View style={{}}>
                         <TouchableOpacity activeOpacity={.3} style={{ flexDirection: 'row', alignItems: 'center', }} onPress={() => setNote(true) + setSelectedIndex(index) + setNoteCheck(true)}>
                             <Text style={{ color: '#000', fontFamily: "OpenSans-Medium", marginRight: 6, fontSize: 14, }}>הוספת הערה</Text>
-                            <AntDesign name={note && index == selectedIndex ? 'edit' : 'plus'} size={14} color={'#000'} style={{ marginRight: 6 }} />
+                            {/* <AntDesign name={note && index == selectedIndex ? 'edit' : 'plus'} size={14} color={'#000'} style={{ marginRight: 6 }} /> */}
+
+                            {(item.note == "" && selectedIndex != index) &&
+                                <AntDesign name={'plus'} size={14} color={'#000'} style={{ marginRight: 6 }} />}
+
+                            {((item.note != "" && selectedIndex != index) || (note && index == selectedIndex)) &&
+                                <AntDesign name={'edit'} size={14} color={'#000'} style={{ marginRight: 6 }} />}
+
                         </TouchableOpacity>
                     </View>
                 </View>
 
-                {vaccineDetails && index == selectedIndex ?
+                {/* {vaccineDetails && index == selectedIndex ? */}
+                {
+                    selectedOpenBox == index &&
                     <View style={{ marginLeft: 20, marginRight: 47, paddingBottom: 20, backgroundColor: '#F2F3F7', borderRadius: 5, marginTop: 15 }}>
                         <View style={{ marginTop: 20, marginRight: 20 }}>
                             <Text style={{ color: '#000', fontFamily: "OpenSans-Bold", marginRight: 6, fontSize: 14, }}>דרכי הדבקה רלוונטיות</Text>
@@ -118,8 +271,7 @@ export default function Home({ navigation }) {
                             <Text style={{ color: '#000', fontFamily: "OpenSans-Medium", marginRight: 6, fontSize: 14, marginLeft: 20 }}>{item.decs4}</Text>
                         </View>
                     </View>
-                    :
-                    null}
+                }
 
                 <View
                     style={{
@@ -135,6 +287,7 @@ export default function Home({ navigation }) {
 
     return (
         <View style={{ flex: 1, backgroundColor: '#fff' }}>
+            {loading && <LoadingPage/>}
             <View style={{ elevation: 5, backgroundColor: '#fff' }}>
                 <Text style={{ textAlign: 'center', color: '#000', fontFamily: "OpenSans-Bold", fontSize: 18, marginTop: 20, marginBottom: 13 }}>פנקס החיסונים</Text>
             </View>
@@ -174,7 +327,9 @@ export default function Home({ navigation }) {
                     marginTop: 20
                 }}
             />
-
+            {
+                console.log("-------->>> flat list data---->>>>" + JSON.stringify(data))
+            }
             <View style={{ flex: 1 }}>
                 <FlatList
                     data={data}
--- src/views/signup/Signup.js
+++ src/views/signup/Signup.js
@@ -32,76 +32,108 @@ export default function Signup({ navigation }) {
 
     const DATA = [
         {
-            id: 'bd7acbea-c1b1-46c2-aed5-3ad53abb28ba',
+            id: 1,
             title: 'שפעת',
             color: '#EBA9C1',
             decs1: 'המצאות בקרבת חולי שפעת.',
             decs2: 'מנה אחת כל שנה.',
             decs3: 'פעיל למשך שנה ויש לחדש כל שנה.',
             decs4: 'הזנים המבוססים על החיסון משתנים כל שנה.',
+            note: '',
+            dose1: false,
+            dose2: false,
+            dose3: false
         },
         {
-            id: '3ac68afc-c605-48d3-a4f8-fbd91aa97f634',
+            id: 2,
             title: '(חצבת, אדמת, חזרת) MMR',
             color: '#EBA9C1',
             decs1: 'מאוד מדבק, דרך חלקיקי אויר במקום שהיית החולה ואף מעט לאחר יציאתו מאיזור ההדבקה.',
             decs2: '2 מנות במהלך החיים.',
             decs3: 'ילידי 57-69 אינם מחוסנים בשגרה, ילידי 70-77 מחוסנים בחיסון אחד בלבד. ילידי 78 והלאה מחוסנים בחיסון מלא על פי תוכנית החיסונים.',
             decs4: 'מומלץ בעיקר לנוסעים לחו״ל ובכל מקרה להיוועץ ברופא המשפחה.',
+            note: '',
+            dose1: false,
+            dose2: false,
+            dose3: false
         },
         {
-            id: '58694a0f-3da1-471f-bd96-145571e29d725',
+            id: 3,
             title: 'צהבת B',
             color: '#EBA9C1',
             decs1: 'מגע מיני לא מוגן, דם.',
             decs2: '3 מנות. 0, 1, 6 חודשים.',
             decs3: 'פעיל לפחות 20 שנה. ילידי שנת 1992 והלאה חוסנו כחלק מחיסוני הילדות. ניתן לבדוק נוגדנים בבדיקת דם בקופת החולים.',
             decs4: 'רלוונטי מאוד לאנשים הנוטלים PREP. יש לוודא חיסוניות לפני התחלת הטיפול.',
+            note: '',
+            dose1: false,
+            dose2: false,
+            dose3: false
         },
         {
-            id: '58694a0f-3da1-471f-bd96-145571e29d7223',
+            id: 4,
             title: 'צהבת A',
             color: '#EBA9C1',
             decs1: 'רימינג , אוכל ומים הנגועים בוירוס.',
             decs2: '2 מנות בהפרש של חצי שנה לפחות.',
             decs3: 'פעיל לפחות 30 שנה. ילידי שנת 1998 והלאה חוסנו כחלק מחיסוני הילדות. ניתן לבדוק בדיקת נוגדים בקופות החולים.',
             decs4: 'שכיחות התפרצויות סביב אירועי גאווה.',
+            note: '',
+            dose1: false,
+            dose2: false,
+            dose3: false
         },
         {
-            id: '58694a0f-3da1-471f-bd96-145571e29d722',
+            id: 5,
             title: 'דלקת ריאות פנאומוקוקלית וסיבוכיה',
             color: '#376BA5',
             decs1: 'המצאות ממושכת וקרובה בסביבת אנשים הנושאים את החיידק בגרונם.',
             decs2: 'למדוכאי חיסון או מעל גיל 65 - 2 סוגי חיסון,.PPSV23 וחודשיים לאחר מכן PCV13',
             decs3: 'פעיל ל5 שנים לפחות - PPSV23 PCV13 - חיסון אחד לכל החיים',
             decs4: 'מתחת לגיל 65 מיועד לקבוצות סיכון מיוחדות בלבד - יש להיוועץ ברופא המטפל.',
+            note: '',
+            dose1: false,
+            dose2: false,
+            dose3: false
         },
         {
-            id: '58694a0f-3da1-471f-bd96-145571e29d72456',
+            id: 6,
             title: 'דלקת קרום המח מנינגוקוקלית B',
             color: '#376BA5',
             decs1: 'המצאות ממושכת וקרובה בסביבת אנשים הנושאים את החיידק בגרונם.',
             decs2: '2 מנות בהפרש של חודש.',
             decs3: 'פעיל ל-5 שנים.',
             decs4: 'הזן הנפוץ ביותר בישראל וברוב מדינות המערב. מחלה עם אחוזי תמותה גבוהים. עלולות להיות  התפרצויות בעיקר באנשים השוהים במקומות סגורים (מסיבות וכ׳ו).',
+            note: '',
+            dose1: false,
+            dose2: false,
+            dose3: false
         },
         {
-            id: '58694a0f-3da1-471f-bd96-145571e29d7211',
+            id: 7,
             title: 'דלקת קרום המח מנינגוקוקלית ACWY',
             color: '#376BA5',
             decs1: 'המצאות ממושכת וקרובה בסביבת אנשים הנושאים את החיידק בגרונם.',
             decs2: 'קבוצות סיכון מיוחדות - 2 מנות בהפרש של חודש. ללא גורמי סיכון - מנה אחת בלבד.',
             decs3: 'פעיל ל-5 שנים. חלק משגרת החיסונים לחיילים מתגייסים.',
             decs4: 'מחלה נדירה אך מאוד מסוכנת.',
+            note: '',
+            dose1: false,
+            dose2: false,
+            dose3: false
         },
         {
-            id: '58694a0f-3da1-471f-bd96-145571e29d721123e4',
+            id: 8,
             title: 'דלקת קרום המח מנינגוקוקלית ACWY',
             color: '#009E69',
             decs1: 'מגע עורי, בעיקר במגע מיני. קונדום אינו מספק הגנה מלאה מפני הדבקה.',
             decs2: '3 מנות על פני חצי שנה.',
             decs3: 'ניתן כסדרה אחת פעם אחת בחיים. כיום ניתן לכל הבנים והבנות בכיתה ח׳.',
             decs4: 'החיסון מיועד למניעת יבלות באברי המין וכן סרטן פי הטבעת, סרטן הפין וסרטן הפה והלוע. ככל  שמבוצע מוקדם יותר כך יעילותו גוברת (הסיכוי לחשיפה קודמת נמוך יותר).',
+            note: '',
+            dose1: false,
+            dose2: false,
+            dose3: false
         },
     ];
 
@@ -182,11 +214,11 @@ export default function Signup({ navigation }) {
 
             <View style={{ flex: 1, justifyContent: 'flex-end', marginBottom: 30 }}>
                 <View style={{ alignSelf: 'center', marginRight: 20, marginTop: 34, marginBottom: 20, flexDirection: 'row', alignItems: 'center', justifyContent: 'flex-end' }}>
-                    <TouchableOpacity onPress={() => { setPrivacyScreen(true)  }}>
+                    <TouchableOpacity onPress={() => { setPrivacyScreen(true) }}>
                         <Text style={{ color: '#000', fontFamily: "OpenSans-Bold", fontSize: 16, marginRight: 5 }}>לתנאי השימוש</Text>
                     </TouchableOpacity>
                     <Text style={{ color: '#000', fontFamily: "OpenSans-Medium", fontSize: 16, marginRight: 10 }}>אני מסכים</Text>
-                    <TouchableOpacity onPress={() => setPrivacyCheck(!privacyCheck)+ setPrivacypolicyVal(false)} style={{}}>
+                    <TouchableOpacity onPress={() => setPrivacyCheck(!privacyCheck) + setPrivacypolicyVal(false)} style={{}}>
                         <MaterialCommunityIcons name={privacyCheck ? 'checkbox-marked-outline' : 'checkbox-blank-outline'} style={{}} size={32} color={privacypolicyVal ? '#FF4040' : '#000'} />
                     </TouchableOpacity>
                 </View>
